506cfaecc1f436fdd7aefaafee59e31ec31096ce
Tai Rodrig <taicapra@gmail.com>
2020-07-07 14:17:33 -0700

    adding another mode (polar coords)

--- uniforms.js
+++ uniforms.js
@@ -79,6 +79,48 @@ const fragmentSrc = `
             gl_FragColor = vec4(r,g,b,alpha);
             //gl_FragColor = texture2D(uSampler2, vec2(x + 0.1*sin(time), y));
 
+        //float distance2edge = abs(dx - 0.5) - 0.5;
+        //gl_FragColor = vec4(0,1,0,pow(distance2edge * 2.0, 0.2));
+    }`;
+
+
+    const lineFragmentSrc = `
+
+    precision mediump float;
+
+    varying vec2 vUvs;
+    varying vec4 position;
+
+    uniform sampler2D uSampler2;
+    uniform float time;
+    uniform float circle_size;
+
+    void main() {
+
+        float dx = vUvs.x - 0.5;
+        float dy = vUvs.y - 0.5;
+
+        float x = mod(vUvs.x * 1.0,1.0);
+        float y = 1.0 - mod(vUvs.y * 1.0,1.0);
+
+        vec4 color = vec4(1,1,1,0);
+
+        if (abs(y * circle_size * 20.0 / 360.0 - x) < 0.01){
+            color = vec4(1,0,0,1);
+        }
+
+        float r = sqrt(dx*dx + dy*dy);
+        
+        float theta = degrees(atan(dy,dx));
+
+        float spiral = mod(theta - circle_size * 20.0 * r, 360.0);
+        float amod = mod(theta+time-120.0*log(r), 30.0) ;
+        if (spiral < 20.0){
+            color = vec4( 0.0, 0.0, 0.0, 1.0 );
+        }
+        
+        gl_FragColor = color;
+
     }`;
 
 let leaves = PIXI.Texture.from('leaves.jpg');
@@ -89,9 +131,10 @@ const uniforms = {
     circle_size: 65,
 };
 
-const shader = PIXI.Shader.from(vertexSrc, fragmentSrc, uniforms);
+const circleShader = PIXI.Shader.from(vertexSrc, fragmentSrc, uniforms);
+const lineShader = PIXI.Shader.from(vertexSrc, lineFragmentSrc, uniforms);
 
-const quad = new PIXI.Mesh(geometry, shader);
+const quad = new PIXI.Mesh(geometry, circleShader);
 
 quad.position.set(w/2, h/2);
 quad.scale.set(4);
@@ -126,6 +169,7 @@ function resize() {
   // area, this is more useful than view.width/height because
   // it handles resolution
   quad.position.set(app.screen.width/2, app.screen.height/2);
+  quad.shape
 
   var ratio = app.screen.width / app.screen.height;
 
@@ -144,3 +188,23 @@ resize();
 screen.orientation.addEventListener("change", function(e) {
     resize();
   }, false);
+
+let keysPressed = {};
+
+document.addEventListener('keydown', (event) => {
+    keysPressed[event.key] = true;
+
+    if (keysPressed['Alt'] && event.key == '1') {
+        alert("Mode 1");
+        quad.shader = circleShader;
+    }
+
+    if (keysPressed['Alt'] && event.key == '2') {
+        alert("Mode 2");
+        quad.shader = lineShader;
+    }
+});
+ 
+ document.addEventListener('keyup', (event) => {
+    delete keysPressed[event.key];
+ });
